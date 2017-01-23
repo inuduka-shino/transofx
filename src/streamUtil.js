fc42188de3f9068dc9649d621f0e6ce6ead46f87
n.m.mobile <hihinuma@gmail.com>
2017-01-24 08:18:39 +0900

    ??? streamUtil

--- src/streamUtil.js
+++ src/streamUtil.js
@@ -4,12 +4,17 @@
 
 function readStreamPromise(strm) {
   return new Promise((resolve, reject) => {
-    const chunk_list = [];
+    const chunk_list = [],
+          options = {};
     strm.on('data', (chunk)=>{
         chunk_list.push(chunk);
     });
     strm.on('end', ()=>{
-      resolve(chunk_list.join(''));
+      if (typeof options.xxxx === 'undefined') {
+        resolve(chunk_list.join(''));
+      } else {
+        resolve(chunk_list);
+      }
     });
     strm.on('error', (err)=>{
       reject(err);
