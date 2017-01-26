c62ae0767881d7bfc94b560be2a89119ef80ee68
n.m.mobile <hihinuma@gmail.com>
2017-01-27 08:08:02 +0900

    snb read

--- test/test_SNB.js
+++ test/test_SNB.js
@@ -7,53 +7,69 @@ const expect = require('chai').expect,
       csvparse = require('csv-parse'),
       streamUtil = require('../src/streamUtil');
 
+function skipHeader(objStrm) {
+  return objStrm;
+}
+function readCSV(csvPath, option = {}) {
+
+  let strm = fs.createReadStream(csvPath);
+
+  if (option.decode) {
+    strm = strm.pipe(iconv.decodeStream(option.decode));
+  }
+  strm = strm.pipe(csvparse({
+            columns: option.field_list,
+          }));
+  if (option.header) {
+    strm = skipHeader(strm);
+  }
+
+  return strm;
+}
 
 describe('SNB Imageテスト', ()=>{
   const
         //workFolderPath = 'test/work',
-        sampleFolderPath = 'test/work_sample',
-        title_flag = true,
-        title_list = [
-          '日付',
-          '内容',
-          '出金金額(円)',
-          '入金金額(円)',
-          '残高(円)',
-          'メモ',
-        ];
+        sampleFolderPath = 'test/work_sample';
 
-  function readCSV(csvPath, option = {}) {
-
-    const inputStrm = fs.createReadStream(csvPath),
-          outputStrm = csvparse({
-                          columns: true,
-                        });
-    let decodedStrm = inputStrm;
-    
-    if (option.decode) {
-      decodedStrm = inputStrm.pipe(iconv.decodeStream(option.decode));
-    }
-    decodedStrm.pipe(outputStrm);
-
-    return outputStrm;
-  }
   describe('SNB CSV read テスト', ()=>{
-    const snbSampleCSVPath = sampleFolderPath + '/snb.csv';
+    const snbSampleCSVPath = sampleFolderPath + '/snb.csv',
+          field_list = [
+            'date',
+            'contents',
+            'payment',
+            'income',
+            'balance',
+            'memo',
+          ];
+
 
     it('SNB CSV read',()=> {
       return co(function *() {
         const rStrm = readCSV(snbSampleCSVPath,{
           decode: 'shift-jis',
+          header: true,
+          field_list,
+          title_list: [
+            '日付',
+            '内容',
+            '出金金額(円)',
+            '入金金額(円)',
+            '残高(円)',
+            'メモ',
+          ],
         });
 
         const rdata = yield streamUtil.readStreamPromise(
                       rStrm, {
-                        objectMode: true
+                        objectMode: true,
                       });
+
         console.log(rdata[0]);
+        console.log(rdata[1]);
         rdata.forEach((data_elm)=>{
-          title_list.forEach((title)=>{
-            expect(data_elm).has.property(title);
+          field_list.forEach((field)=>{
+            expect(data_elm).has.property(field);
           });
         });
       });
