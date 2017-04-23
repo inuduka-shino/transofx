1b5a195008a297d423e917aabd62e540659f52b5
nknshm <hihinuma@gmail.com>
2017-04-23 16:58:33 +0900

    add pre-cli-test

---
+++ test/test_total.js
@@ -0,0 +1,62 @@
+/*eslint-env node, mocha */
+
+const expect = require('chai').expect,
+      co = require('co'),
+      bankFile = require('../src/bankFileUtil'),
+      streamUtil = require('../src/streamUtil'),
+      {
+      //  orderedDictToStream,
+        makeOfxObjStream,
+      } = require('../src/ofx_stream_construct');
+
+
+describe('SNB SAMBLE', ()=>{
+  const
+        //workFolderPath = 'test/work',
+        sampleFolderPath = 'test/work_sample';
+
+  describe('sample CSV read ', ()=>{
+    const sampleCSVPath = sampleFolderPath + '/nyushukinmeisai_20170423_SBI.csv',
+      fieldList = [
+        'date',
+        'contents',
+        'payment',
+        'income',
+        'balance',
+        'memo',
+      ],
+      titleList = [
+        '日付',
+        '内容',
+        '出金金額(円)',
+        '入金金額(円)',
+        '残高(円)',
+        'メモ',
+      ],
+      sbiCSVOption = {
+        csvPath: sampleCSVPath,
+        encode: 'shift-jis',
+        header: true,
+        headerCheck: true,
+        fieldList,
+        titleList
+      };
+
+    it('SBI csv',() => {
+      return co(function *() {
+        const rStrm = bankFile.readCSV(sbiCSVOption);
+        //makeOfxObjStream
+
+
+        const rdata = yield streamUtil.readStreamPromise(
+          rStrm,
+          {
+            objectMode: true
+          }
+        );
+
+        expect(rdata).is.equal('aaaa');
+      });
+    });
+  });
+});
